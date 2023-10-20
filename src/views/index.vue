fc55939428b369105b33d36b5515d933933c4011
kerian <18700872228@163.com>
2023-10-20 20:56:27 +0800

    add: reset

---
+++ src/views/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-header />
+	<div class="main-bot">
+		<div class="main-content-box">
+			<router-view v-slot="{ Component }">
+				<transition name="move" mode="out-in">
+					<keep-alive>
+						<component :is="Component"></component>
+					</keep-alive>
+				</transition>
+			</router-view>
+		</div>
+	</div>
+</template>
+<script setup lang="ts" name="index">
+import vHeader from '@c/header/';
+
+</script>
+<style lang="less" scoped>
+.main-bot {
+	flex: 1;
+	display: flex;
+	height: 100%;
+}
+
+.main-content-box {
+	flex: 1;
+	height: 100%;
+	overflow: hidden;
+}
+</style>
